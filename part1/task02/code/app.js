1188a5e83d67ec2af9fc74712ef5476504d6afd0
iceCream <2187215483@qq.com>
2020-05-28 23:16:22 +0800

    first update part_01-task02 code and notes

--- part1/task02/code/app.js
+++ part1/task02/code/app.js
@@ -1,1 +1,29 @@
-console.log("success");
+function once(fn){
+    let done = false;
+    return function(){
+        if(done) return ;
+        done = true;
+        return fn.apply(this, arguments);
+    }
+}
+
+let pay = once((money) => {
+    console.log(money);
+});
+
+pay(18);
+pay(18);
+pay(18);
+
+
+const some = (arr, fn) => {
+    let result = false;
+    for(let item of arr){
+        result = fn(item);
+        if(result) break;
+    }
+    return result;
+}
+let arr = [1,3,4,9];
+let r = some(arr, v => v % 2 === 0);
+console.log(r);
