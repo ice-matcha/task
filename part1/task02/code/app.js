2d557600b236e074e654fbd6cb6e3fc122db4da1
iceCream <2187215483@qq.com>
2020-05-31 21:49:01 +0800

    add new notes

--- part1/task02/code/app.js
+++ part1/task02/code/app.js
@@ -1,3 +1,4 @@
+/*
 function once(fn){
     let done = false;
     return function(){
@@ -27,3 +28,25 @@ const some = (arr, fn) => {
 let arr = [1,3,4,9];
 let r = some(arr, v => v % 2 === 0);
 console.log(r);
+*/
+
+//const _ = require("lodash");
+
+function getSum(a , b, c){
+    return a + b + c;
+}
+
+//const gs = _.curry(getSum);
+const gs = curry(getSum);
+
+function curry(fn){
+    return curriedFn = function(...args){
+        if(args.length >= fn.length) return fn(...args);
+        return function() {
+            return curriedFn(...args.concat(Array.from(arguments)));
+        }
+    }
+}
+
+console.log(gs(1,2)(3));
+console.log(gs(1,2,3));
